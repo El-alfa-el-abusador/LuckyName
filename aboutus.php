72fb91e419892224078136ebba5d83dc2b9e64c3
aoliver10 <57540163+aoliver10@users.noreply.github.com>
2021-02-23 20:16:58 +0100

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="favicon.gif" type="image/gif">
+    <link href="css/custom.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Caesar+Dressing&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="js/custom.js"></script>
+    <title>LuckyName</title>
+</head>
+
+<body>
+    <header class="encabezado">
+        <div id="logo" class="logo d-flex justify-content-center" title="Logo"></div>
+    </header>
+
+    <main>
+        <div class="container-fluid">
+        <div id="contenido" class="row d-flex justify-content-center">
+        <div id="contacto" class="col-md-5">
+        <form id="contacto" class="my-3 p-3 rounded sombra h-100 needs-validation bg-white rounded" validate>
+            <div class="col-md">
+            texto sin nadafjfjfdjfj
+            
+            </div>
+        </form>
+    </main>
+    <footer>
+        <div id="oscuro" class="dot" title="Modo Dark Vader">
+            <span id="icono" class="fa fa-moon fa-2x"></span>
+        </div>
+    </footer>
+</body>
+
+</html>
